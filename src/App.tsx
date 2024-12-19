00840ffb6284eae478b42497593aa03bed9f9556
toufiq007 <t.limon2342@gmail.com>
2024-12-20 00:29:26 +0600

    setup our weather app with typescript and intall shadcn ui library

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="bg-red-500">weather app</h2>
+      <Button>click me</Button>
+    </>
+  );
+}
+
+export default App;
